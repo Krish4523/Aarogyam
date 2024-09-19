5c46f5bb32f8d8341076ec3f5e8779f156aee90e
MansiMalaviya8 <mansimalaviya26@gmail.com>
2024-09-19 17:39:03 +0530

    feat(aarogyam-client):Add hospital, medical examination in admin

--- client/aarogyam-client/src/components/ui/badge.tsx
+++ client/aarogyam-client/src/components/ui/badge.tsx
@@ -1,36 +1,36 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { HTMLAttributes } from "react";
 
-import { cn } from "@/lib/utils"
-
+// Define a utility function for creating badge variants using `cva`
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center px-2 py-1 rounded-full text-xs font-semibold",
   {
     variants: {
       variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
-        outline: "text-foreground",
+        solid: "text-white bg-red-500", // Solid style for a red badge
+        subtle: "text-red-500 bg-red-100", // Subtle style
+      },
+      size: {
+        small: "text-xs", // Small size
+        medium: "text-sm", // Medium size
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "solid",
+      size: "small",
     },
   }
-)
+);
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
+  extends HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+export function Badge({ className, variant, size, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+    <div className={cn(badgeVariants({ variant, size }), className)} {...props}>
+      {props.children}
+    </div>
+  );
 }
-
-export { Badge, badgeVariants }
